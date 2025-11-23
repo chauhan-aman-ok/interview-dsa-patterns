856921731c29c45ed29973fdc1103b0fa8d7d56f
Aman-Chauhan-ok <chauhanaman9688@gmail.com>
2025-11-23 19:47:17 +0530

    count distinct element in every k size window
    Implement a method to count distinct elements in each sliding window of size k.

---
+++ arrays/slidingwindow/problem5/distinct.java
@@ -0,0 +1,45 @@
+//problem 5 - Count distinct elements in every window
+// topics - fixed-size sliding window, hashmap
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class distinct {
+    public static ArrayList<Integer> countDistinct(int arr[], int k){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        ArrayList<Integer> res = new ArrayList<>();
+
+        for(int i=0;i<k;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }else{
+                map.put(arr[i],1);
+            }
+        }
+        res.add(map.size());
+        int i=1;
+        int j=k;
+
+        while(j<arr.length){
+
+
+            if(j<arr.length){
+                if(map.containsKey(arr[j])){
+                    map.put(arr[j],map.get(arr[j])+1);
+                }else{
+                    map.put(arr[j],1);
+                }
+                if(map.get(arr[i-1])==1){
+                    map.remove(arr[i-1]);
+                }else{
+                    map.put(arr[i-1],map.get(arr[i-1])-1);
+                }
+            }
+            res.add(map.size());
+            i++;j++;
+
+        }
+        return res;
+    }
+}

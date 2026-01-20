e6faf79748c46a4aa9aae79658bb4ea49a5e3f09
Aman-Chauhan-ok <chauhanaman9688@gmail.com>
2026-01-20 09:38:02 +0530

    Implement removeElement function using two pointers

---
+++ arrays/TwoPointer/problem8/remElement.java
@@ -0,0 +1,17 @@
+// leetcode - 27
+// topic - slow fast two pointer
+// Remove element
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i=0,j=0;
+        while(j<nums.length){
+            if(nums[j]!=val){
+                nums[i++]=nums[j];
+            }
+            j++;
+        }
+        return i;
+    }
+}
+
+

fae97ef69e3631433ff1c4f22f801d95455ebe6b
Aman-Chauhan-ok <chauhanaman9688@gmail.com>
2026-02-21 14:22:29 +0530

    Add insertIntoBST function for BST insertion
    Implement insertIntoBST function to insert a value into a binary search tree.

---
+++ Trees/BST/level-0-BST-basics/insertintoBST.cpp
@@ -0,0 +1,13 @@
+// leetcode 701, insert into a bst
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==nullptr){
+            return new TreeNode(val);
+        }
+        if(root->val>val) root->left=insertIntoBST(root->left,val);
+        else root->right=insertIntoBST(root->right,val);
+        return root;
+    }
+};

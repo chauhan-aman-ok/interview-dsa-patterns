46190e6420e2a2dd74df3769c5e21eb27695b565
Aman-Chauhan-ok <chauhanaman9688@gmail.com>
2026-03-15 23:38:57 +0530

    Update recursion.cpp

--- dynamic-programming/level-2-knapsack/unbounded-knapsack/problem1-unbounded-knapsack/recursion.cpp
+++ dynamic-programming/level-2-knapsack/unbounded-knapsack/problem1-unbounded-knapsack/recursion.cpp
@@ -14,10 +14,10 @@ class Solution {
 int unbound(vector<int> &val,vector<int>& wt,int bag,int idx){
     if(idx==val.size() || bag<=0) return 0;
     if(wt[idx]<=bag){
-        int pick=val[idx]+unbound(val,wt,bag-wt[idx],idx,dp);
-        int skip=unbound(val,wt,bag,idx+1,dp);
+        int pick=val[idx]+unbound(val,wt,bag-wt[idx],idx);
+        int skip=unbound(val,wt,bag,idx+1);
         return max(pick,skip);
-    }else return unbound(val,wt,bag,idx+1,dp);
+    }else return unbound(val,wt,bag,idx+1);
 }
     int knapSack(vector<int>& val, vector<int>& wt, int bag) {
         // code here
